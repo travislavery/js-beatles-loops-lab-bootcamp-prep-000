7a83d58c30d5e4631aa29db6de811cf973ba5280
Travis Lavery <travisalavery@gmail.com>
2017-11-21 02:56:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,18 @@ function johnLennonFacts(factsArray) {
   var empty = []
   while(arrayLength >= 0) {
     var nextString = factsArray[arrayLength] + "!!!"
+    empty.push(nextString)
     arrayLength -= 1
   }
+  return empty
+}
+
+function iLoveTheBeatles(number) {
+  var empty = []
+  var parameter = 15
+  do {
+    empty.push('I love the Beatles!')
+    parameter -= 1
+  } while (parameter > 0)
+  return empty
 }
