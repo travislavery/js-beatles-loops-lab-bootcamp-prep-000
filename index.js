eae3268a54bf4928bc81e3e4c0f9737724d9cfe5
Travis Lavery <travisalavery@gmail.com>
2017-11-21 02:44:25 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,7 @@
+function theBeatlesPlay(musicians, instruments) {
+  var empty = []
+  for (var i = 0; i < musicians.length-1; i++) {
+    var nextString = `${musicians[i]} plays ${instruments[i]}`
+    
+  }
+}
